be9ff2522a38799dbfb048d5c9d55350a52e09ce
Panu Ohra-aho <panu.ohraaho@gmail.com>
2026-01-27 13:09:28 +0200

    Language changer partially implemented

---
+++ tool_showcase/tools/language/language.js
@@ -0,0 +1,88 @@
+class LanguageOption {
+	name;
+	text;
+	title;
+
+	constructor(name, text, title) {
+		this.name = name;
+		this.text = text;
+		if(title != null) this.title = title;
+	}
+}
+
+class Language extends HTMLElement {
+	text = [];
+	current_language = "";
+	target_element;
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.FindTargetElement();
+		const toggler = document.querySelector("language-toggler");
+		if(toggler != null) {
+			this.setAttribute("default_language", toggler.getAttribute("languages").split(" ")[0])
+			this.current_language = toggler.getAttribute("default_language");
+		}
+
+		this.text.push(
+			new LanguageOption(
+				this.getAttribute("default_language"), 
+				this.target_element.innerHTML, 
+				this.target_element.getAttribute("title")
+			)
+		);
+		for(let i = 0; i < this.children.length; i++) {
+			this.text.push(
+				new LanguageOption(
+					this.children[i].getAttribute("name"), 
+					this.children[i].innerHTML, 
+					this.children[i].getAttribute("title")
+				)
+			);
+		}
+		this.innerHTML = "";
+		this.GetCurrentLanguage();
+	}
+
+	ChangeLanguage(language) {
+		this.current_language = language;
+		for(let i = 0; i < this.text.length; i++) {
+			if(this.current_language == this.text[i].name) {
+				if(this.target_element.getAttribute("title") && this.text[i].title != null) { 
+					this.target_element.setAttribute("title", this.text[i].title)
+				}
+
+				this.target_element.innerText = this.text[i].text;
+				break;
+			}
+		}
+	}
+
+	GetCurrentLanguage() {
+		const l = window.sessionStorage.getItem("language");
+		if(l != this.current_language) {
+			this.ChangeLanguage(l);
+		}
+	}
+
+	FindTargetElement() {
+		let temp = this.previousSibling;
+		let safe = 0;
+
+		//Loop through white spaces and texts
+		while(temp.nodeName == "#text") {
+			temp = temp.previousSibling;
+			safe++;
+			if(safe >= 100) {
+				console.log("safe triggered");
+				break;
+			}
+		}
+		this.target_element = temp;
+	}
+
+}
+
+customElements.define("language-changer", Language);
---
+++ tool_showcase/tools/language/language_toggler.js
@@ -0,0 +1,82 @@
+const language_toggler_template = document.createElement("template");
+language_toggler_template.innerHTML = 
+`
+<button>
+</button>
+`
+
+class LanguageToggler extends HTMLElement { 
+	current_language = "";
+	languages = [];
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		const l = window.sessionStorage.getItem("language");
+		this.CollectLanguages();
+		this.SetDefaultLanguages();
+		
+		if(l != null && l != undefined) {
+			this.current_language = l;
+		} else {
+			this.current_language = this.languages[0];
+			window.sessionStorage.setItem("language", this.current_language);
+		}
+		let template = language_toggler_template.content.cloneNode(true);
+		template.children[0].innerHTML = this.current_language;
+		template.children[0].addEventListener("click", () => {
+			this.ChangeLanguage();
+		});
+
+		//Set button text
+		let current_index = this.languages.indexOf(this.current_language);
+		current_index++;
+		if(current_index >= this.languages.length) {
+			current_index = 0;
+		}
+		template.children[0].innerHTML = this.languages[current_index];
+		
+		this.appendChild(template);
+	}
+
+	ChangeLanguage() {
+		
+		let current_index = this.languages.indexOf(this.current_language);
+		current_index++;
+		if(current_index >= this.languages.length) {
+			current_index = 0;
+		}
+		this.current_language = this.languages[current_index];
+
+		if(current_index+1 >= this.languages.length) {
+			this.children[0].innerHTML = this.languages[0];
+		} else {
+			this.children[0].innerHTML = this.languages[current_index+1];
+		}
+
+		const l_cs = document.querySelectorAll("language-changer");
+		for(let i = 0; i < l_cs.length; i++) {
+			l_cs[i].ChangeLanguage(this.current_language);
+		}
+
+		window.sessionStorage.setItem("language", this.current_language);
+	}
+
+	SetDefaultLanguages() {
+		const l_cs = document.querySelectorAll("language-changer");
+		for(let i = 0; i < l_cs.length; i++) {
+			l_cs[i].setAttribute("default_language", this.languages[0]);
+		}
+	}
+
+	CollectLanguages() {
+		const proto_languages = this.getAttribute("languages");
+		this.languages = proto_languages.split(" ");
+	}
+
+	
+
+}
+
+customElements.define("language-toggler", LanguageToggler);
